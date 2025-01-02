d3a26c8367ea6d23507fac22fd24ead412578d09
fmkiko <fmkiko@gmail.com>
2025-01-02 15:58:34 +0100

    fix: allocation of resources

--- server.js
+++ server.js
@@ -2,13 +2,24 @@ process.env["NODE_TLS_REJECT_UNAUTHORIZED"]=0;
 const express = require('express');
 const path = require('path');
 const { OpenAI_API } = require('./openai');
+const rateLimit = require('express-rate-limit');
 
 
 const app = express();
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutos
+    max: 100, // Máximo 100 solicitudes por ventana de tiempo por IP
+    message: 'Demasiadas solicitudes desde esta IP, inténtelo de nuevo en 15 minutos',
+    standardHeaders: true, // Retorna encabezados `RateLimit-*` en la respuesta
+    legacyHeaders: false, // Deshabilita los encabezados `X-RateLimit-*`
+  });
+
 const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
+app.use(limiter);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
